d27455752dc3926a1105818421c8dc2ce2c31dc8
Tan Zong Xuan <tzxules@gmail.com>
2010-11-14 14:33:13 +0800

    Simple checkerdraw function added.

--- checkergen.py
+++ checkergen.py
@@ -1,6 +1,12 @@
 import os, sys
 import pygame
 from pygame.locals import *
+
+def checkerdraw(Surface, position, board_dims, cell_dims, cols):
+    for i in [cell_dims[0]*x for x in range(board_dims[0])]:
+        for j in [cell_dims[1]*x for x in range(board_dims[1])]:
+            pygame.draw.rect(Surface, cols[(i+j)%2], (position[0]+i,position[1]+j) + cell_dims)
+
 pygame.init()
 
 size = width, height = 800, 600
@@ -9,7 +15,10 @@ pygame.display.set_caption('checkergen')
 screen = pygame.display.get_surface()
 clock = pygame.time.Clock()
 
-pygame.draw.rect(screen, Color(255,255,255), (200,150,400,300))
+black = Color(0,0,0,0)
+white = Color(255,255,255,255)
+
+checkerdraw(screen, (width/4,height/4), (width/50,height/50), (25,25), (black,white))
 
 while True:
     clock.tick(60)
