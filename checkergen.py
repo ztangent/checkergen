c6d62d2d9594ae22756114e1d5834fb2313ce541
Tan Zong Xuan <tzxules@gmail.com>
2010-11-18 00:42:00 +0800

    Checkerdraw now capable of distortion.

--- checkergen.py
+++ checkergen.py
@@ -2,10 +2,24 @@ import os, sys
 import pygame
 from pygame.locals import *
 
-def checkerdraw(Surface, position, board_dims, cell_dims, cols):
-    for i in [cell_dims[0]*x for x in range(board_dims[0])]:
-        for j in [cell_dims[1]*x for x in range(board_dims[1])]:
-            pygame.draw.rect(Surface, cols[(i+j)%2], (position[0]+i,position[1]+j) + cell_dims)
+def checkerdraw(Surface, position, board_dims, init_cell_dims, end_cell_dims, cols):
+    cur_cell_pos = list(position)
+    cell_grad = [(end_cell_dims[x]-init_cell_dims[x])/board_dims[x] for x in range(2)]
+    cur_cell_dims = [init_cell_dims[x]+cell_grad[x]/2 for x in range(2)]
+    for j in range(board_dims[1]):
+        for i in range(board_dims[0]):
+            pygame.draw.rect(Surface, cols[(i+j)%2], tuple(cur_cell_pos + cur_cell_dims))
+            #Increase x values
+            cur_cell_pos[0] += cur_cell_dims[0]
+            cur_cell_dims[0] += cell_grad[0]
+        #Reset x values
+        cur_cell_pos[0] = position[0]
+        cur_cell_dims[0] = init_cell_dims[0]+cell_grad[0]/2
+        #Increase y values
+        cur_cell_pos[1] += cur_cell_dims[1]
+        cur_cell_dims[1] += cell_grad[1]
+
+
 
 pygame.init()
 
@@ -18,7 +32,7 @@ clock = pygame.time.Clock()
 black = Color(0,0,0,0)
 white = Color(255,255,255,255)
 
-checkerdraw(screen, (width/4,height/4), (width/50,height/50), (25,25), (black,white))
+checkerdraw(screen, (width/4,height/4), (10,10), (10,10), (50,50), (black,white))
 
 while True:
     clock.tick(60)
