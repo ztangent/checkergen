8499e51b893c2ca63b15506b36508ba82a9ba5b1
Tan Zong Xuan <tzxrules@gmail.com>
2010-11-22 18:52:57 +0800

    position a CheckerBoard attribute, draw and anim improvements. Made position an attribute of CheckerBoard, optionally redefinable in draw and anim. Also made anim draw properly on first run, and made phasing work properly.

--- checkergen.py
+++ checkergen.py
@@ -2,23 +2,35 @@ import os, sys
 import pygame
 from pygame.locals import *
 
+DEFAULT_FPS = 60
+
+BLACK = Color(0,0,0)
+GREY = Color(127,127,127)
+WHITE = Color(255,255,255) 
+
 CB_ORIGIN = {'topleft': (1,1), 'topright': (-1,1), 'btmleft': (1,-1), 'btmright': (-1,-1)}
+
+global_fps = DEFAULT_FPS
 
 class CheckerBoard:
 
-    def __init__(self, dims, init_unit, end_unit, origin, cols, freq, phase = 0):
+    def __init__(self, dims, init_unit, end_unit, position, origin, cols, freq, phase = 0):
         self.dims = dims
         self.init_unit = [float(x) for x in init_unit]
         self.end_unit = [float (x) for x in end_unit]
         self.unit_grad = [(self.end_unit[x] - self.init_unit[x]) / self.dims[x] for x in range(2)]
+        self.position = list(position)
         self.origin = origin
-        self.cols = cols
+        self.cols = list(cols)
         self.freq = float(freq)
         self.phase = float(phase) # In degrees
+        self.firstrun = True
 
-    def draw(self, Surface, position):
+    def draw(self, Surface, position = None):
         # Set initial values
-        cur_unit_pos = list(position)
+        if position != None:
+            self.position = list(position)
+        cur_unit_pos = self.position[:]
         cur_unit = [self.init_unit[x] + (self.unit_grad[x] / 2) for x in range(len(self.init_unit))]
         for j in range(self.dims[1]):
             for i in range(self.dims[0]):
@@ -32,19 +44,24 @@ class CheckerBoard:
                 cur_unit_pos[0] += CB_ORIGIN[self.origin][0]*cur_unit[0]
                 cur_unit[0] += self.unit_grad[0]
             # Reset x values
-            cur_unit_pos[0] = position[0]
+            cur_unit_pos[0] = self.position[0]
             cur_unit[0] = self.init_unit[0] + (self.unit_grad[0] / 2)
             # Increase y values
             cur_unit_pos[1] += CB_ORIGIN[self.origin][1]*cur_unit[1]
             cur_unit[1] += self.unit_grad[1]
 
-    def anim(self, Surface, position, fps):
+    def anim(self, Surface, position = None, fps = None):
+        if fps == None:
+            fps = global_fps
         fpp = fps / self.freq
-        self.phase += 360 / fpp
+        if self.firstrun == True:
+            self.firstrun = False
+        else:
+            self.phase += 360 / fpp
         if self.phase >= 180:
             self.phase -= 180
             self.cols.reverse()
-            self.draw(Surface, position)
+        self.draw(Surface, position)
 
 pygame.init()
 
@@ -53,30 +70,21 @@ window = pygame.display.set_mode(size)
 pygame.display.set_caption('checkergen')
 screen = pygame.display.get_surface()
 
-fps = 30
 clock = pygame.time.Clock()
-
-BLACK = Color(0,0,0)
-GREY = Color(127,127,127)
-WHITE = Color(255,255,255) 
 
 screen.fill(GREY)
  
-myboard1 = CheckerBoard([6, 6], [20, 20], [40, 40], 'btmright', [BLACK, WHITE], 1)
-myboard2 = CheckerBoard([6, 6], [20, 20], [40, 40], 'btmleft', [BLACK, WHITE], 2)
-myboard3 = CheckerBoard([6, 6], [20, 20], [40, 40], 'topright', [BLACK, WHITE], 3)
-myboard4 = CheckerBoard([6, 6], [20, 20], [40, 40], 'topleft', [BLACK, WHITE], 4)
-myboard1.draw(screen, (width/2 - 20, height/2 - 20))
-myboard2.draw(screen, (width/2 + 20, height/2 - 20))
-myboard3.draw(screen, (width/2 - 20, height/2 + 20))
-myboard4.draw(screen, (width/2 + 20, height/2 + 20))
+myboard1 = CheckerBoard([6, 6], [20, 20], [40, 40], [width/2 - 20, height/2 - 20], 'btmright', [BLACK, WHITE], 1)
+myboard2 = CheckerBoard([6, 6], [20, 20], [40, 40], [width/2 + 20, height/2 - 20], 'btmleft', [BLACK, WHITE], 2)
+myboard3 = CheckerBoard([6, 6], [20, 20], [40, 40], [width/2 - 20, height/2 + 20], 'topright', [BLACK, WHITE], 3)
+myboard4 = CheckerBoard([6, 6], [20, 20], [40, 40], [width/2 + 20, height/2 + 20], 'topleft', [BLACK, WHITE], 4)
 
 while True:
-    clock.tick(fps)
-    myboard1.anim(screen, (width/2 - 20, height/2 - 20), fps)
-    myboard2.anim(screen, (width/2 + 20, height/2 - 20), fps)
-    myboard3.anim(screen, (width/2 - 20, height/2 + 20), fps)
-    myboard4.anim(screen, (width/2 + 20, height/2 + 20), fps)
+    clock.tick(global_fps)
+    myboard1.anim(screen)
+    myboard2.anim(screen)
+    myboard3.anim(screen)
+    myboard4.anim(screen)
     for event in pygame.event.get():
         if event.type == QUIT:
             sys.exit(0)
