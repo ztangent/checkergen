205c49e9fbb49625b57d3649dcd5e44bcf01640e
ZOMGxuan <tzxules@gmail.com>
2011-01-05 12:02:03 +0800

    Fixed global variable referencing in signals, send 0 to parallel port at exit.

--- signals.py
+++ signals.py
@@ -38,28 +38,40 @@ except ImportError:
 if available['serial']:
     def ser_init():
         global SERPORT
+        global SERSTATE
+        global SERFLIP
+        SERFLIP = False
+        SERSTATE = 0
         SERPORT = serial.Serial(0)
 
     def ser_set_on():
         global SERSTATE
+        global SERFLIP
         if SERSTATE != GROUP_ON:
             SERFLIP = True
             SERSTATE = GROUP_ON
         
     def ser_set_off():
         global SERSTATE
+        global SERFLIP
         if SERSTATE != GROUP_OFF:
             SERFLIP = True
             SERSTATE = GROUP_OFF
 
     def ser_send():
         global SERPORT
+        global SERSTATE
+        global SERFLIP
         if SERFLIP:
             SERPORT.write(str(SERSTATE))
             SERFLIP = False
 
     def ser_quit():
         global SERPORT
+        global SERSTATE
+        global SERFLIP
+        SERFLIP = False
+        SERSTATE = 0
         SERPORT.close()
         SERPORT = None
 else:
@@ -82,28 +94,42 @@ else:
 if available['parallel']:
     def par_init():
         global PARPORT
+        global PARSTATE
+        global PARFLIP
+        PARSTATE = 0
+        PARFLIP = False
         PARPORT = parallel.Parallel()
+        PARPORT.setData(PARSTATE)
 
     def par_set_on():
         global PARSTATE
+        global PARFLIP
         if PARSTATE != GROUP_ON:
             PARFLIP = True
             PARSTATE = GROUP_ON
         
     def par_set_off():
         global PARSTATE
+        global PARFLIP
         if PARSTATE != GROUP_OFF:
             PARFLIP = True
             PARSTATE = GROUP_OFF
 
     def par_send():
         global PARPORT
+        global PARSTATE
+        global PARFLIP
         if PARFLIP:
             PARPORT.setData(PARSTATE)
             PARFLIP = False
 
     def par_quit():
         global PARPORT
+        global PARSTATE
+        global PARFLIP
+        PARSTATE = 0
+        PARFLIP = False
+        PARPORT.setData(PARSTATE)
         PARPORT = None
 else:
     msg = 'parallel port functionality not available'
