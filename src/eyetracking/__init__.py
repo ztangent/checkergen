690cfa3a49dfdbf2e9ce987464f23bb6aee87e64
ZOMGxuan <tzxrules@gmail.com>
2011-02-02 17:20:03 +0800

    Added init function.

--- src/eyetracking/__init__.py
+++ src/eyetracking/__init__.py
@@ -1,5 +1,21 @@
 """Provides support for CRS VideoEyetracker Toolbox."""
 
 import win32com.client
+try:
+    import CRS
+except ImportError:
+    import crsheadergen
+    crsheadergen.generate()
+    import CRS
+    del crsheadergen
 
-vet = win32com.client.Dispatch("VET.CRSVideoEyetracker")
+vet = None
+ProgID = "crsVET.VideoEyeTracker"
+
+def init(source=CRS.vsHighSpeedCamera250):
+    global vet
+    vet = win32com.client.Dispatch(ProgID)
+    vet.SelectVideoSource(source, '')
+    vet.StartTracking()
+    vet.CreateCameraScreen(0)
+    
--- src/eyetracking/crsheadergen.py
+++ src/eyetracking/crsheadergen.py
@@ -16,7 +16,9 @@ import sys
 
 mfilename = 'crsLoadConstants.m'
 pyfilename = 'CRS.py'
-default_path = os.path.normpath('C:\Program Files\icannotremember')
+default_path = os.path.normpath(\
+"C:\Program Files\Cambridge Research Systems\
+\CRS Toolbox for MATLAB\Tools\VSG\System\crsLoadConstants.m")
 sep1 = "\
 % *************************************\
 ****************************************\n\
@@ -32,34 +34,45 @@ sep2 = "\
 % *************************************\
 ****************************************"
 
-print 'path to crsLoadConstants.m: (default: {0})'.format(default_path)
-path = raw_input()
-if len(path) == 0:
-    # Default to standard Windows install path
-   path == default_path
-else:
-    path = os.path.normpath(path)
-if not os.path.isfile(path):
-    sys.exit("path specified is not a file")
-elif os.path.basename(path) != mfilename:
-    sys.exit("wrong filename, {0} expected".format(mfilename))
+def generate():
 
-# U flag for standardized newlines
-mfile = open(path, 'rU')
-mstr = mfile.read()
-mfile.close()
+   print 'path to crsLoadConstants.m: (default: {0})'.format(default_path)
+   path = raw_input()
+   if path == '':
+       # Default to standard Windows install path
+       path = default_path
+   else:
+       path = os.path.normpath(path)
+   print path
+   if not os.path.isfile(path):
+       sys.exit("path specified is not a file")
+   elif os.path.basename(path) != mfilename:
+       sys.exit("wrong filename, {0} expected".format(mfilename))
 
-# Actual text processing
-pystr = mstr.split(sep1)[1]
-pystr = sep1 + pystr
-pystr = pystr.split(sep2)[0]
-pystr = pystr.replace('%', '#')
-pystr = pystr.replace('CRS.', '')
-pystr = pystr.replace(';', '')
+   # U flag for standardized newlines
+   mfile = open(path, 'rU')
+   mstr = mfile.read()
+   mfile.close()
 
-# Write out to file
-pyfile = open(pyfilename, 'w')
-pyfile.write(pystr)
-pyfile.close()
+   # Actual text processing
+   pystr = mstr.split(sep1)[1]
+   pystr = sep1 + pystr
+   pystr = pystr.split(sep2)[0]
+   pystr = pystr.replace('%', '#')
+   pystr = pystr.replace('CRS.', '')
+   pystr = pystr.replace(';', '')
 
-print 'Done.'
+   # Remove leading whitespace
+   pystrlist = pystr.splitlines()
+   pystrlist = [line.strip() for line in pystrlist]
+   pystr = '\n'.join(pystrlist)
+
+   # Write out to file
+   pyfile = open(pyfilename, 'w')
+   pyfile.write(pystr)
+   pyfile.close()
+
+   print 'Done.'
+
+if __name__ == '__main__':
+   generate()
